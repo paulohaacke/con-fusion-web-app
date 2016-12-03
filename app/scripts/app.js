1d5a163f1f21a079574316be87f6701137d8aa36
Paulo André Haacke <paulohaacke@gmail.com>
2016-12-03 23:40:05 +0000

    Files updated according to assignment 3 tasks.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,64 +1,71 @@
 'use strict';
 
 angular.module('confusionApp', ['ui.router'])
-    .config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
-        // route for the home page
+        
+            // route for the home page
             .state('app', {
-                url: '/',
+                url:'/',
                 views: {
                     'header': {
-                        templateUrl: 'views/header.html'
+                        templateUrl : 'views/header.html',
                     },
                     'content': {
-                        template: '<h1>To be Completed</h1>',
-                        controller: 'IndexController'
+                        templateUrl : 'views/home.html',
+                        controller  : 'IndexController'
                     },
                     'footer': {
-                        templateUrl: 'views/footer.html'
+                        templateUrl : 'views/footer.html',
                     }
                 }
+
             })
+        
             // route for the aboutus page
             .state('app.aboutus', {
-                url: 'aboutus',
+                url:'aboutus',
                 views: {
                     'content@': {
-                        template: '<h1>To be Completed</h1>'
+                        templateUrl : 'views/aboutus.html',
+                        controller  : 'AboutController'                  
                     }
                 }
             })
+        
             // route for the contactus page
             .state('app.contactus', {
-                url: 'contactus',
+                url:'contactus',
                 views: {
                     'content@': {
-                        templateUrl: 'views/contactus.html',
-                        controller: 'ContactController'
+                        templateUrl : 'views/contactus.html',
+                        controller  : 'ContactController'                  
                     }
                 }
             })
 
-        // route for the menu page
-        .state('app.menu', {
-            url: 'menu',
-            views: {
-                'content@': {
-                    templateUrl: 'views/menu.html',
-                    controller: 'MenuController'
+            // route for the menu page
+            .state('app.menu', {
+                url: 'menu',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/menu.html',
+                        controller  : 'MenuController'
+                    }
                 }
-            }
-        })
+            })
 
-        // route for the dishdetail page
-        .state('app.dishdetails', {
-            url: 'menu/:id',
-            views: {
-                'content@': {
-                    templateUrl: 'views/dishdetail.html',
-                    controller: 'DishDetailController'
+            // route for the dishdetail page
+            .state('app.dishdetails', {
+                url: 'menu/:id',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/dishdetail.html',
+                        controller  : 'DishDetailController'
+                   }
                 }
-            }
-        });
+            });
+    
         $urlRouterProvider.otherwise('/');
     })
+;
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,122 +1,99 @@
 'use strict';
 
 angular.module('confusionApp')
-    .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
 
-        $scope.tab = 1;
-        $scope.filtText = '';
-        $scope.showDetails = false;
+        .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+            
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
 
-        $scope.dishes = menuFactory.getDishes();
+            $scope.dishes= menuFactory.getDishes();
 
-        $scope.select = function(setTab) {
-            $scope.tab = setTab;
-
-            if (setTab === 2) {
-                $scope.filtText = "appetizer";
-            }
-            else if (setTab === 3) {
-                $scope.filtText = "mains";
-            }
-            else if (setTab === 4) {
-                $scope.filtText = "dessert";
-            }
-            else {
-                $scope.filtText = "";
-            }
-        };
-
-        $scope.isSelected = function(checkTab) {
-            return ($scope.tab === checkTab);
-        };
-
-        $scope.toggleDetails = function() {
-            $scope.showDetails = !$scope.showDetails;
-        };
-    }])
-
-.controller('ContactController', ['$scope', function($scope) {
-
-    $scope.feedback = {
-        mychannel: "",
-        firstName: "",
-        lastName: "",
-        agree: false,
-        email: ""
-    };
-
-    var channels = [{
-        value: "tel",
-        label: "Tel."
-    }, {
-        value: "Email",
-        label: "Email"
-    }];
-
-    $scope.channels = channels;
-    $scope.invalidChannelSelection = false;
-
-}])
-
-.controller('FeedbackController', ['$scope', function($scope) {
-
-    $scope.sendFeedback = function() {
-
-        console.log($scope.feedback);
-
-        if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
-            $scope.invalidChannelSelection = true;
-            console.log('incorrect');
-        }
-        else {
-            $scope.invalidChannelSelection = false;
-            $scope.feedback = {
-                mychannel: "",
-                firstName: "",
-                lastName: "",
-                agree: false,
-                email: ""
+                        
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+                
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
             };
-            $scope.feedback.mychannel = "";
-            $scope.feedbackForm.$setPristine();
-            console.log($scope.feedback);
-        }
-    };
-}])
 
-.controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
-    var dish = menuFactory.getDish(parseInt($stateParams.id, 10));
-    $scope.dish = dish;
-}])
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+    
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
 
-.controller('DishCommentController', ['$scope', function($scope) {
+        .controller('ContactController', ['$scope', function($scope) {
 
-    $scope.comment = {
-        author: "",
-        rating: 5,
-        comment: ""
-    };
-    $scope.ratings = [1, 2, 3, 4, 5];
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+                        
+        }])
 
-    //Step 1: Create a JavaScript object to hold the comment from the form
-    $scope.submitComment = function() {
+        .controller('FeedbackController', ['$scope', function($scope) {
+            
+            $scope.sendFeedback = function() {
+                
+                console.log($scope.feedback);
+                
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
 
-        //Step 2: This is how you record the date
-        $scope.comment.date = new Date().toISOString();
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-        // Step 3: Push your comment into the dish's comment array
-        $scope.dish.comments.push($scope.comment);
+            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
+            
+            $scope.dish = dish;
+            
+        }])
 
-        //Step 4: reset your form to pristine
-        $scope.commentForm.$setPristine();
+        .controller('DishCommentController', ['$scope', function($scope) {
+            
+            $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            
+            $scope.submitComment = function () {
+                
+                $scope.mycomment.date = new Date().toISOString();
+                console.log($scope.mycomment);
+                
+                $scope.dish.comments.push($scope.mycomment);
+                
+                $scope.commentForm.$setPristine();
+                
+                $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            }
+        }])
 
-        //Step 5: reset your JavaScript object that holds your comment
-        $scope.comment = {
-            author: "",
-            rating: 5,
-            comment: ""
-        };
-    }
-}])
+        // implement the IndexController and About Controller here
+
 
 ;

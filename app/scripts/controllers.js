0243d42864a173a75757df2c6a316a37503bf7aa
Paulo André Haacke <paulohaacke@gmail.com>
2016-12-04 02:43:34 +0000

    Fixing identation

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -123,17 +123,13 @@ angular.module('confusionApp')
 
 // implement the IndexController and About Controller here
 .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
-
     $scope.featuredDish = menuFactory.getDish(0);
     $scope.promotion = menuFactory.getPromotion(0);
     $scope.chefExecutive = corporateFactory.getLeader(3);
-
 }])
 
 .controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory) {
-
     $scope.leadersList = corporateFactory.getLeaders();
-
 }])
 
 ;

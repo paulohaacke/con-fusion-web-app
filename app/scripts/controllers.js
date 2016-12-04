6192726d8727bf6d4766a7e3c8e6c70531222d5e
Paulo André Haacke <paulohaacke@gmail.com>
2016-12-04 22:54:29 +0000

    Handling errors in client-server communication using .

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,11 +8,17 @@ angular.module('confusionApp')
     $scope.filtText = '';
     $scope.showDetails = false;
 
-    $scope.dishes = [];
+    $scope.showMenu = false;
+    $scope.message = "Loading ...";
+    $scope.dishes = {};
     menuFactory.getDishes()
         .then(
             function(response) {
                 $scope.dishes = response.data;
+                $scope.showMenu = true;
+            },
+            function(response) {
+                $scope.message = "Error: " + response.status + " " + response.statusText;
             }
         );
 
@@ -93,16 +99,19 @@ angular.module('confusionApp')
 }])
 
 .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
-
     $scope.dish = {};
+    $scope.showDish = false;
+    $scope.message = "Loading ...";
     menuFactory.getDish(parseInt($stateParams.id, 10))
         .then(
             function(response) {
                 $scope.dish = response.data;
                 $scope.showDish = true;
+            },
+            function(response) {
+                $scope.message = "Error: " + response.status + " " + response.statusText;
             }
         );
-
 }])
 
 .controller('DishCommentController', ['$scope', function($scope) {
@@ -135,11 +144,17 @@ angular.module('confusionApp')
 // implement the IndexController and About Controller here
 .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
     $scope.featuredDish = {};
+    $scope.showDish = false;
+    $scope.message = "Loading ...";
+
     menuFactory.getDish(0)
         .then(
             function(response) {
                 $scope.featuredDish = response.data;
                 $scope.showDish = true;
+            },
+            function(response) {
+                $scope.message = "Error: " + response.status + " " + response.statusText;
             }
         );
     $scope.promotion = menuFactory.getPromotion(0);

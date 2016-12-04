37c3df4a264f1fcd36f8580e1f3ca29525b1be9a
Paulo André Haacke <paulohaacke@gmail.com>
2016-12-04 22:37:31 +0000

    Client-Server communication usign  service exercise done.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,7 +8,13 @@ angular.module('confusionApp')
     $scope.filtText = '';
     $scope.showDetails = false;
 
-    $scope.dishes = menuFactory.getDishes();
+    $scope.dishes = [];
+    menuFactory.getDishes()
+        .then(
+            function(response) {
+                $scope.dishes = response.data;
+            }
+        );
 
 
     $scope.select = function(setTab) {
@@ -88,9 +94,14 @@ angular.module('confusionApp')
 
 .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-    var dish = menuFactory.getDish(parseInt($stateParams.id, 10));
-
-    $scope.dish = dish;
+    $scope.dish = {};
+    menuFactory.getDish(parseInt($stateParams.id, 10))
+        .then(
+            function(response) {
+                $scope.dish = response.data;
+                $scope.showDish = true;
+            }
+        );
 
 }])
 
@@ -123,7 +134,14 @@ angular.module('confusionApp')
 
 // implement the IndexController and About Controller here
 .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
-    $scope.featuredDish = menuFactory.getDish(0);
+    $scope.featuredDish = {};
+    menuFactory.getDish(0)
+        .then(
+            function(response) {
+                $scope.featuredDish = response.data;
+                $scope.showDish = true;
+            }
+        );
     $scope.promotion = menuFactory.getPromotion(0);
     $scope.chefExecutive = corporateFactory.getLeader(3);
 }])
